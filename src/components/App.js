60b92698fbd488a45a15ec7307ce623ad644d118
Benjamin Odisho <Semiroundpizza8@gmail.com>
2022-10-29 09:13:14 -0500

    Init Kermit

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+/* 
+    This is you entry point for your routes
+*/
+const App = () => {
+    return (
+        <div>
+            <h1>Hello World</h1>
+        </div>
+    );
+};
+
+export default App;
